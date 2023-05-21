53f52c745931b235858c4be02ffa3ad4f88c28d5
MatveyZh404 <118770204+MatveyZh404@users.noreply.github.com>
2023-05-21 22:36:47 +0300

    Add files via upload

---
+++ Консоль Лабораторная работа №4/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] arr = { 1, -2, 3, -4, 5, -6, 7, -8, 9, -10 };
+int sum = 0;
+
+foreach (int num in arr)
+{
+    if (num > 0)
+    {
+        sum += num;
+    }
+}
+
+Console.WriteLine("Сумма положительных элементов массива: " + sum);
